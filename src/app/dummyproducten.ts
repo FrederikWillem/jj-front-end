9f4e5e4085709fd59b35f5f00e403ab6b06e6043
Frederik040 <Frederik.Vink@unit040.com>
2022-07-26 21:27:16 +0200

    visualize dummy data in list

--- src/app/dummyproducten.ts
+++ src/app/dummyproducten.ts
@@ -1,12 +1,12 @@
 import { ProductData } from "./product-data";
 
 export const dummyProduct : ProductData[] = [
-    new ProductData(){
+    {
         Naam : "Oorbel",
         Material: "Roestvrij staal",
         Kleur: "Goud",
         Categorie: "Oorbellen",
         Prijs: 10.00,
-        FotoURL: "http://localhost:8081/JJfotos/oorbel.jpg"
-    }
+        FotoURL: "http://localhost:8081/JJfotos/oorbel.jpg",
+    },
 ];
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { dummyProduct } from '../dummyproducten';
+import { ProductData } from '../product-data';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  @Input()
+  producten : ProductData[] = dummyProduct;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public get amountOfProduct(): number {
+    return this.producten.length;
+  }
+
+  public get productsFirstColumn(): ProductData[] {
+    return this.producten;
   }
 
 }
